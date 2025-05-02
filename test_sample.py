2768b28d60c4a9bfe8dd7261f3320d7393dab1d1
Mariem <mariem.landoulsi2002@gmail.com>
2025-05-02 05:38:18 +0100

    eme comm

---
+++ test_sample.py
@@ -0,0 +1,10 @@
+# test_sample.py
+
+def test_addition():
+    assert 1 + 1 == 2  # Test de base pour vérifier l'addition
+
+def test_subtraction():
+    assert 2 - 1 == 1  # Test pour vérifier la soustraction
+
+def test_multiplication():
+    assert 2 * 3 == 6  # Test pour vérifier la multiplication
