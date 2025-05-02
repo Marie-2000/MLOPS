d2b9346668b596089985a8a5825e4caf41a3b3c2
Mariem <mariem.landoulsi2002@gmail.com>
2025-05-02 06:29:16 +0100

    em

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ def predict():
     return f"<h3>Résultat : {result}</h3><a href='/'>↩ Retour</a>"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
